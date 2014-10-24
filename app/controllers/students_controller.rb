7be2014ac95818b80c318a4754b504195d72dabc
frozenfung <frozenfung@gmail.com>
2014-10-24 21:28:08 +0800

    do partial on edit and new, also add flash

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,26 +16,31 @@ before_action :set_student, :only => [:show, :edit, :update, :destroy]
 
   def create
     @student = Student.new(student_params)
-    @student.save
-
-    redirect_to students_path
+    if @student.save
+      flash[:create_success] = @student.name + ' is added Successfully!'
+      redirect_to students_path
+    else
+      render :action => :new
+    end
   end
 
   def edit
   end
 
   def update
-    @student.update_attributes(student_params)
-
-    redirect_to student_path(@student)
+    if @student.update_attributes(student_params)
+      flash[:update_success] = 'Updated Successfully!'
+      redirect_to student_path(@student)
+    else
+      render :action => :edit
+    end
   end
 
 
   def destroy
+    flash[:delete_success] = @student.name + ' is Deleteeeeeeeeeed!'
     @student.destroy
-
     redirect_to students_path
-
   end
 
 
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,3 @@
 class Student < ActiveRecord::Base
+  validates_presence_of :name, :gender, :student_class, :status
 end
