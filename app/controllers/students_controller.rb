069c48e46b542465b63863b81439e40d4abd747c
frozenfung <frozenfung@gmail.com>
2014-10-24 15:29:12 +0800

    add edit, update, destroy action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,13 @@
 class StudentsController < ApplicationController
 
+before_action :set_student, only => [:show, :edit, :update, :destroy]
+
   def index
     @students = Student.all
   end
 
   def show
-    @student = Student.find(params[:id])
+
   end
 
   def new
@@ -17,6 +19,23 @@ class StudentsController < ApplicationController
     @student.save
 
     redirect_to students_path
+  end
+
+  def edit
+  end
+
+  def update
+    @student.update_attributes(student_params)
+
+    redirect_to student_path(@student)
+  end
+
+
+  def destroy
+    @student.destroy
+
+    redirect_to students_path
+
   end
 
   def student_params
